3b883ef27082e41a7224651d5fc4662715e0d29e
Cédric Belin <cedx@outlook.com>
2026-01-25 14:29:24 +0100

    Add the `Table` attribute

---
+++ src/DataAnnotations/Table.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+namespace Belin\Sql\DataAnnotations;
+
+/**
+ * Specifies the database table that a class is mapped to.
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class Table {
+
+	/**
+	 * The name of the table the class is mapped to.
+	 */
+	public readonly string $name;
+
+	/**
+	 * The schema of the table the class is mapped to.
+	 */
+	public readonly ?string $schema;
+
+	/**
+	 * Creates a new table attribute.
+	 * @param string $name The name of the table the class is mapped to.
+	 * @param string $schema The schema of the table the class is mapped to.
+	 */
+	public function __construct(string $name, ?string $schema = null) {
+		$this->name = $name;
+		$this->schema = $schema;
+	}
+}
